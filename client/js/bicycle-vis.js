230694c732d5a7f1d5029812debd6daeef0198a0
Saad Ismail <sismail@Saads-MacBook-Pro.local>
2014-11-12 00:09:59 -0500

    implemented basic cell selection

--- client/js/bicycle-vis.js
+++ client/js/bicycle-vis.js
@@ -4,14 +4,17 @@ var dataMatrixXAxis;
 function init() {
 	randomData = generateRandomDataForCategoryDataMatrix(null, 8, 8, 1500, 300);
 	dataMatrix = new CategoryDataMatrix("#data-matrix-container", randomData, 300, 300);
+	//dataMatrix.updateDataset(randomData);
 	dataMatrix.initDataMatrix();
 	
 	randomDataForYAxis = generateRandomDataForCategoryDataMatrix(null, 8, 1, 1500, 300);
 	dataMatrixYAxis = new CategoryDataMatrix("#data-matrix-yaxis-container", randomDataForYAxis, 37.5, 300);
+	//dataMatrix.updateDataset(randomDataForYAxis);
 	dataMatrixYAxis.initDataMatrix();
 	
 	randomDataForXAxis = generateRandomDataForCategoryDataMatrix(null, 1, 8, 1500, 300);
 	dataMatrixXAxis = new CategoryDataMatrix("#data-matrix-xaxis-container", randomDataForXAxis, 300, 37.5);
+	//dataMatrix.updateDataset(randomDataForXAxis);
 	dataMatrixXAxis.initDataMatrix();
 	
 	initTimeSlider();
--- client/js/vis-components/category-data-matrix.js
+++ client/js/vis-components/category-data-matrix.js
@@ -1,25 +1,57 @@
 function CategoryDataMatrix(elementName, cells, widthAttr, heightAttr) { // constructor function
-	
-	var width = widthAttr, 
+	var width = widthAttr,
 		height = heightAttr;
+	var cellStrokeColor = "#7e7e7e",
+		selectedCellStrokeColor = "#ffff00";
 	var matrixDataset = cells;
 	var numRows = matrixDataset.length,
 		numCols = matrixDataset[0].length,
 		data = null,
 		color = d3.interpolateRgb("#fff", "#f00");
-	var selectCell = function(cell) {
+	var selectedCells;
+	
+	var toggleCellSelection = function(cell, data) {
+			if(selectedCells[data.row][data.col] == false)
+			{
+				selectCell(cell, data);
+			}
+			else
+			{
+				unselectCell(cell, data);
+			}
+		}
+	var selectCell = function(cell, data) {
+			selectedCells[data.row][data.col] = true;
+			d3.select(cell).attr("stroke", selectedCellStrokeColor).attr("stroke-width", 1);
+			cell.parentNode.parentNode.appendChild(cell.parentNode);
+			cell.parentNode.appendChild(cell);
+		};
+	var unselectCell = function(cell, data) {
+			selectedCells[data.row][data.col] = false;
+			d3.select(cell).attr("stroke", cellStrokeColor).attr("stroke-width", 1);
+		};
+		
+	var highlightCell = function(cell, data) {
 			d3.select(cell).attr("stroke", "#f00").attr("stroke-width", 3);
 			cell.parentNode.parentNode.appendChild(cell.parentNode);
 			cell.parentNode.appendChild(cell);
 		};
-	var deselectCell = function(cell) {
-			d3.select(cell).attr("stroke", "#fff").attr("stroke-width", 1);
+	var unhighlightCell = function(cell, data) {
+			if(selectedCells[data.row][data.col] == true)
+			{
+				d3.select(cell).attr("stroke", selectedCellStrokeColor).attr("stroke-width", 1);
+			}
+			else
+			{
+				d3.select(cell).attr("stroke", cellStrokeColor).attr("stroke-width", 1);
+			}
 		};
+		
 	var onCellOver = function(cell, data) {
-			selectCell(cell);
+			//highlightCell(cell, data);
 		};
 	var onCellOut = function(cell, data) {
-			deselectCell(cell);
+			//unhighlightCell(cell, data);
 		};
 	var createHeatchart = function() {
 			var min = 999;
@@ -37,7 +69,6 @@ function CategoryDataMatrix(elementName, cells, widthAttr, heightAttr) { // cons
 				}
 			}
 			var tooltip = d3.select("body").append("div").style("position", "absolute").style("z-index", "10").style("visibility", "hidden").text("a simple tooltip");
-			//setup x & y axes
 			var heatchart = d3.select(elementName).append("svg:svg").attr("width", width).attr("height", height);
 			heatchart.selectAll("g").data(matrixDataset).enter().append("svg:g").selectAll("rect").data(function(d) {
 				return d;
@@ -47,7 +78,7 @@ function CategoryDataMatrix(elementName, cells, widthAttr, heightAttr) { // cons
 				return d.row * (height / numRows);
 			}).attr("width", width / numCols).attr("height", height / numRows).attr("fill", function(d, i) {
 				return color((d.points.length - min) / (max - min));
-			}).attr("stroke", "#fff").attr("cell", function(d) {
+			}).attr("stroke", cellStrokeColor).attr("cell", function(d) {
 				return "r" + d.row + "c" + d.col;
 			}).on("mouseover", function(d) {
 				onCellOver(this, d);
@@ -59,11 +90,11 @@ function CategoryDataMatrix(elementName, cells, widthAttr, heightAttr) { // cons
 				onCellOut(this, d);
 				tooltip.style("visibility", "hidden");
 			}).on("click", function(d) {
-				console.log("clicking: [" + d.col + "," + d.row + "]");
+				//console.log("clicking: [" + d.col + "," + d.row + "]");
+				toggleCellSelection(this,d);
 			});
 		};
 	this.updateHeatchart = function() {
-		console.log("update heat chart");
 		var min = 999;
 		var max = -999;
 		var l;
@@ -78,6 +109,7 @@ function CategoryDataMatrix(elementName, cells, widthAttr, heightAttr) { // cons
 				}
 			}
 		}
+		var tooltip = d3.select("body").append("div").style("position", "absolute").style("z-index", "10").style("visibility", "hidden").text("a simple tooltip");
 		d3.select(elementName).select("svg").selectAll("g").data(matrixDataset).selectAll("rect").data(function(d) {
 			return d;
 		}).attr("x", function(d, i) {
@@ -90,20 +122,39 @@ function CategoryDataMatrix(elementName, cells, widthAttr, heightAttr) { // cons
 			return "r" + d.row + "c" + d.col;
 		}).on("mouseover", function(d) {
 			onCellOver(this, d);
+			tooltip.text("Cell: [" + d.col + "," + d.row + "]");
+			tooltip.style("visibility", "visible");
+		}).on("mousemove", function(d) {
+			tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
 		}).on("mouseout", function(d) {
 			onCellOut(this, d);
+			tooltip.style("visibility", "hidden");
 		}).on("click", function(d) {
-			console.log("clicking: [" + d.col + "," + d.row + "]");
+			//console.log("clicking: [" + d.col + "," + d.row + "]");
+			toggleCellSelection(this,d);
 		});
 	};
 	this.initDataMatrix = function() {
 		createHeatchart();
 	};
-	
 	this.updateDataset = function(newCells) {
 		matrixDataset = newCells;
 	}
 	this.getDataset = function() {
 		return matrixDataset;
 	}
+	var createDefaultSelectedCells = function() {
+		selectedCells = new Array(numRows);
+		for (i = 0; i < numRows; i++) {
+	
+	        selectedCells[i] = new Array(numCols);
+	
+	        for (j = 0; j < numCols; j++) {
+	            selectedCells[i][j] = false;
+	        }
+	    }
+		
+	}
+	
+	createDefaultSelectedCells();
 }
