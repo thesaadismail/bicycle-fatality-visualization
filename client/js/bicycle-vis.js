9c16e3764a3b3e4e6e8240109716f5fc75716dd9
Saad Ismail <sismail@Saads-MacBook-Pro.local>
2014-11-11 21:15:22 -0500

    styling/layout updates to index.html and stylesheet.css

--- client/js/bicycle-vis.js
+++ client/js/bicycle-vis.js
@@ -2,7 +2,7 @@ var dataMatrix;
 
 function init() {
 	randomData = generateRandomDataForCategoryDataMatrix(8, 8, 1500, 300);
-	dataMatrix = new CategoryDataMatrix("#heatchartContainer", randomData, 300);
+	dataMatrix = new CategoryDataMatrix("#data-matrix-container", randomData, 300);
 	dataMatrix.initDataMatrix();
 	
 	initTimeSlider();
