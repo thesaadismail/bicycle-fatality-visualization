77d6999755a5e38d99130802dc017c6a514b1e05
hannah-kim <hannah-kim@users.noreply.github.com>
2014-11-23 14:36:20 -0500

    Update timeslider.js

--- client/js/vis-components/timeslider.js
+++ client/js/vis-components/timeslider.js
@@ -1,10 +1,10 @@
-var chart;
+var OVchart;
 
-var margin = {top: 12, right: 30, bottom: 20, left: 55},
-    width = 770 - margin.left - margin.right,
-    height = 70 - margin.top - margin.bottom;
+var OVmargin = {top: 12, right: 30, bottom: 20, left: 55},
+    OVwidth = 770 - OVmargin.left - OVmargin.right,
+    OVheight = 70 - OVmargin.top - OVmargin.bottom;
 
-var parseDate = d3.time.format("%b-%Y").parse;
+var OVparseDate = d3.time.format("%b-%Y").parse;
 
 
 //Gets called when the page is loaded.
@@ -13,67 +13,79 @@ function initTimeSlider(){
   ///////////////////////////////
   // Year Overview Line Graph
   ///////////////////////////////
-  var x = d3.time.scale()
-    .range([0, width]);
+  var OVx = d3.time.scale()
+    .range([0, OVwidth]);
 
-  var y = d3.scale.linear()
-    .range([height, 0]);
+  var OVy = d3.scale.linear()
+    .range([OVheight, 0]);
 
-  var xAxis = d3.svg.axis()
-    .scale(x)
+  var OVxAxis = d3.svg.axis()
+    .scale(OVx)
     .orient("bottom")
     .tickSize(10, 0)
     .tickFormat(d3.time.format("%b"));
 
-  var yAxis = d3.svg.axis()
-    .scale(y)
+  var OVyAxis = d3.svg.axis()
+    .scale(OVy)
     .ticks(4)
     .orient("left");
 
-  var line = d3.svg.line()
-    .x(function(d) { return x(d.key); })
-    .y(function(d) { return y(d.values); });
+  var OVbrush = d3.svg.brush()
+    .x(OVx)
+    .on("brush", OVbrushed);
 
-  var brush = d3.svg.brush()
-    .x(x)
-    .on("brush", brushed);
+  // var line = d3.svg.line()
+  //   .x(function(d) { return x(d.key); })
+  //   .y(function(d) { return y(d.values); });
 
-  var chart = d3.select('#timeslider').append('svg')
+  var OVarea = d3.svg.area()
+    .interpolate("monotone")
+    .x(function(d) { return OVx(d.key); })
+    .y0(height)
+    .y1(function(d) { return OVy(d.values); });
+
+  var OVchart = d3.select('#timeslider').append('svg')
     .attr("style", "outline: thin solid gray;") 
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("width", OVwidth + OVmargin.left + OVmargin.right)
+    .attr("height", OVheight + OVmargin.top + OVmargin.bottom);
 
-  var vis = chart.append('svg:g')
+  OVchart.append("defs").append("clipPath")
+      .attr("id", "clip")
+    .append("rect")
+      .attr("width", OVwidth)
+      .attr("height", OVheight);
+
+  var OVvis = OVchart.append('svg:g')
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   
-  d3.csv('client/data/CoffeeData.csv', function(rawdata) {
-    var format = d3.time.format("%m/%d/%Y");
-    var monthNameFormat = d3.time.format("%m");
-    data_agg = d3.nest()
-          .key(function(d) {return monthNameFormat(format.parse(d.date));})
+  d3.csv('data/CoffeeData.csv', function(rawdata) {
+    var OVformat = d3.time.format("%m/%d/%Y");
+    var OVmonthNameFormat = d3.time.format("%m");
+    OVdata_agg = d3.nest()
+          .key(function(d) {return OVmonthNameFormat(OVformat.parse(d.date));})
           .rollup(function(d) {return d3.sum(d, function(g) {return g.sales;});})
           .entries(rawdata);
 
-    data_agg.forEach(function(d) {
+    OVdata_agg.forEach(function(d) {
       d.key = new Date(2014, d.key-1, 1);
       d.values = +d.values;
     });
 
-    x.domain(d3.extent(data_agg, function(d) { return d.key; }));
-    y.domain(d3.extent(data_agg, function(d) { return d.values; }));
+    OVx.domain(d3.extent(OVdata_agg, function(d) { return d.key; }));
+    OVy.domain(d3.extent(OVdata_agg, function(d) { return d.values; }));
 
-    vis.append("g")
+    OVvis.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
+        .attr("transform", "translate(0," + OVheight + ")")
+        .call(OVxAxis)
         .selectAll(".tick text")
         .style("text-anchor", "start")
         .attr("x", 4)
         .attr("y", 5);
 
-    vis.append("g")
+    OVvis.append("g")
         .attr("class", "y axis")
-        .call(yAxis)
+        .call(OVyAxis)
       .append("text")
         .attr("transform", "rotate(0)")
         .attr("x", -6)
@@ -82,25 +94,28 @@ function initTimeSlider(){
         .style("text-anchor", "end")
         .text("Fatalities");
 
-    vis.append("path")
-        .attr("class", "line")
-        .attr("d", line(data_agg))
-        .attr("stroke-width", 2)
-        .attr("stroke", "steelblue")
-        .attr("fill", "none");
+    OVvis.append("path")
+        .datum(OVdata_agg)
+        .attr("class", "area")
+        .attr("d", OVarea);
 
-    vis.append("g")
+    OVvis.append("g")
+        .attr("class", "x brush")
+        .call(OVbrush)
+      .selectAll("rect")
+        .attr("y", -6)
+        .attr("height", OVheight + 7);
+
+    OVvis.append("g")
         .append("rect")
 
   });
 
-  function brushed() {
-    x.domain(brush.empty() ? x2.domain() : brush.extent());
-    focus.select(".area").attr("d", area);
-    focus.select(".x.axis").call(xAxis);
+  function OVbrushed() {
+    OVbrush.empty() ? tmp = OVx.domain() : tmp = OVbrush.extent();
+    // vis.select(".area").attr("d", area);
+    // vis.select(".x.axis").call(xAxis);
   }
-
-
   
 }
 
