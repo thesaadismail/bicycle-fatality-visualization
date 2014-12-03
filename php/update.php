26dde35ecab46f7dce35e855aaf3a8d3f76110b1
Siddharth <siddharthraja30@gmail.com>
2014-12-02 22:10:49 -0500

    update view on selection change

--- php/update.php
+++ php/update.php
@@ -7,11 +7,26 @@
     $server = mysql_connect($host, $username, $password);
     $connection = mysql_select_db($database, $server);
 	
+	$data = $_POST["result"];
+	$data   = json_decode("$data",true);
+    //just echo an item in the array
+	
+	$selectedStates = "first";
+	
+	for ($x = 1; $x <= 56; $x++) {		//count($data)
+		if($data[$x]!=null && $data[$x]==1)
+			if($selectedStates=="first")
+				$selectedStates = $x;
+			else
+				$selectedStates = $selectedStates.", ".$x;
+	} 
+	echo "V: ".$selectedStates.", ";
 	
 	$myquery="ALTER VIEW current_data 
-AS SELECT statenum, casenum, atmcond, 'crash day', 'crash hr', 'crash min', 'crash mon', 'crash time', 'caseyear', lightcond, nmlocat FROM data_all
-";
-	
+				AS SELECT statenum, casenum, atmcond, 'crash day', 'crash hr', 'crash min', 'crash mon', 'crash time', 'caseyear', lightcond, nmlocat FROM data_all
+				WHERE statenum IN (".$selectedStates.");";
+			echo "br />".$myquery;
+	/* 
 	$query = mysql_query($myquery);
 			
 	if ( ! $query ) {
@@ -20,14 +35,7 @@ AS SELECT statenum, casenum, atmcond, 'crash day', 'crash hr', 'crash min', 'cra
 	}
 	
 	mysql_query($query);
-	echo "query result";
-	/*	
-	for ($x = 0; $x < mysql_num_rows($query); $x++) {
-		$data[] = mysql_fetch_assoc($query);
-	}
-	
-	echo json_encode($data); */
-		
+	echo "query result"; */
 	
     mysql_close($server);
 ?>
