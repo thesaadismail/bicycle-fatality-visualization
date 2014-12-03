7ed8654dd22598562597ab2e0deeb1b7c202921c
Siddharth <siddharthraja30@gmail.com>
2014-12-03 00:07:00 -0500

    db call for cdm location axis

---
+++ php/cdmLocationAxis.php
@@ -0,0 +1,41 @@
+<?php
+	include $_SERVER['DOCUMENT_ROOT'].'/php/ChromePhp.php';
+	ChromePhp::log('Hello console!');
+	//ChromePhp::warn('something went wrong!');
+	
+	
+    $username = "root"; 
+    $password = "";   
+    $host = "localhost";
+    $database="iv";
+    
+    $server = mysql_connect($host, $username, $password);
+    $connection = mysql_select_db($database, $server);
+	
+	
+	$myquery="
+		SELECT nm_location.type Location, count( current_data.casenum ) Number_of_Fatalities
+		FROM current_data, nm_location
+		WHERE nm_location.id = current_data.nmlocat
+		GROUP BY nm_location.type
+		ORDER BY Location ASC 
+	";
+	
+	$query = mysql_query($myquery);
+			
+	if ( ! $query ) {
+		echo mysql_error();
+		die;
+	}
+	
+	$data = array();
+	
+	for ($x = 0; $x < mysql_num_rows($query); $x++) {
+		$data[] = mysql_fetch_assoc($query);
+	}
+	
+	echo json_encode($data);
+		
+	
+    mysql_close($server);
+?>
