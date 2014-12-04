b3ad5ecb252afaa567ae0885842eba83e309aa6e
siddharthraja <siddharthraja30@gmail.com>
2014-12-03 19:12:31 -0500

    timeslider update incorporated

--- php/update.php
+++ php/update.php
@@ -1,6 +1,9 @@
 <?php
 	include 'dbinfo.php';
-    
+    include 'ChromePhp.php';
+	ChromePhp::log('Hello console!');
+	//ChromePhp::warn('something went wrong!');
+	
     $server = mysql_connect($host, $username, $password);
     $connection = mysql_select_db($database, $server);
 	
@@ -9,7 +12,8 @@
     //just echo an item in the array
 	
 	$selectedStates = "first";
-	
+	$startmonth = 1;//$data['start'];
+	$endmonth = 5;//$data['end'];
 	for ($x = 1; $x <= 56; $x++) {		//count($data)
 		if($data[$x]!=null && $data[$x]==1)
 			if($selectedStates=="first")
@@ -23,8 +27,8 @@
 	$myquery="ALTER VIEW current_data 
 				AS SELECT statenum, casenum, atmcond,  accdate, accday, acchr, accmin, accmon, acctime , dayofweek, lightcond, nmlocat
 				FROM data_all
-				WHERE statenum IN (".$selectedStates.");";
-	echo '<br />'.$myquery;
+				WHERE statenum IN (".$selectedStates.") AND accmon BETWEEN ".$startmonth." AND ".$endmonth;
+	ChromePhp::log($myquery);
 	
 	$query = mysql_query($myquery);
 			
